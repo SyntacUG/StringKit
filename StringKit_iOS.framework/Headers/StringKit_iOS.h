549fcf7c7b2f5924c3e0ff0acfd01ba176b29f59
Rainer Niemann <rainerniemann@icloud.com>
2017-06-15 12:29:06 +0200

    StringKit v0.2.0

---
+++ StringKit_iOS.framework/Headers/StringKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  StringKit_iOS.h
+//  StringKit_iOS
+//
+//  Created by Rainer Niemann on 31.05.17.
+//  Copyright © 2017 Syntac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringKit_iOS.
+FOUNDATION_EXPORT double StringKit_iOSVersionNumber;
+
+//! Project version string for StringKit_iOS.
+FOUNDATION_EXPORT const unsigned char StringKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringKit_iOS/PublicHeader.h>
+
+
